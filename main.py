4829711d2eac1d754070195c71e805df064a8ab4
Mist-wu <2550166270@qq.com>
2025-12-02 16:52:30 +0800

    update

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ bot = BotClient()
 @bot.private_event()
 async def on_private_message(msg: PrivateMessage):
     if msg.raw_message == "测试":
-        await bot.api.post_private_msg(msg.user_id, text="NcatBot 测试成功喵~")
+        await bot.api.post_private_msg(msg.user_id, text="NcatBot 测试成功")
 
 # ========== 启动 BotClient==========
 bot.run()
