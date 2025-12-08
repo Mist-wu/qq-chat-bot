6c44e09fef4df784933d95c0d73f384c92c19bda
Mist-wu <2550166270@qq.com>
2025-12-08 10:07:39 +0800

    update

--- weather.py
+++ weather.py
@@ -1,14 +1,14 @@
-# 导入requests库
+import load_dotenv as env
 import requests
 
 # 定义API密钥
-API_KEY = 'PyHRKtUKt1kPJt4Ror2teXeczj'
+API_KEY = env.weather_api_key
  
 # 设置url
 url = f'https://api2.wer.plus/api/weather?key={API_KEY}'
  
 # 发送post请求
-response = requests.post(url, data={'city': '北京市'})
+response = requests.post(url, data={'city': '海淀'})
  
 # 获取响应内容
 result = response.json()
